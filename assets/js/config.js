21c9b648f78ae147e70e5adf14a961e5045a20bb
Carlos Bello <bellopauste@yahoo.com>
2015-10-27 01:36:15 +0100

    Ajustes en estilos para bootstrap3 y corrección de CDN de kendo para usar https

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ requirejs.config({
     paths: {
         app: '../js',
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min',
-        k: '//cdn.kendostatic.com/2015.1.408/js',
+        k: '//kendo.cdn.telerik.com/2015.1.408/js',
         'jquery.easing': '//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min',
         bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min',
         views: '/assets/html',
