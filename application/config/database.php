7534dd976d04fe0ab29eac7ca7c41787f40bad2e
Carlos Bello <cbello@tuenti.com>
2018-09-11 00:09:37 +0200

    Fixes DB access info

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = 'academia';
+$db['default']['password'] = 'academia';
 $db['default']['database'] = 'academia';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
