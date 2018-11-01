afec5876faf01c6f9b016808c286c383b8ae1eea
Carlos Bello <cbello@tuenti.com>
2018-11-01 20:06:40 +0100

    #75 Poject structure reorganization

--- www/index.php
+++ www/index.php
@@ -59,7 +59,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'vendor/rogeriopradoj/codeigniter/system';
+	$system_path = '../vendor/rogeriopradoj/codeigniter/system';
 
 /*
  *---------------------------------------------------------------
